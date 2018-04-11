16122b14c2a0c97cb0950173a6a35a5c14728af8
napierSLAM <37292377+napierSLAM@users.noreply.github.com>
2018-04-11 17:05:51 +0100

    Update test1.ino

--- speedsensor/test1.ino
+++ speedsensor/test1.ino
@@ -6,14 +6,11 @@
   */
 
 
-
 // Constant for Distance Interrupt Pin
 const byte DIST_PIN = 2;
 
 // Volatile int to hold slot counts
 volatile int slotC = 0;
-
-
 
 // Interrupt Service Routine for counting slots
 void ISR_count()
@@ -21,21 +18,14 @@ void ISR_count()
   slotC++;  // increment slot count
 }
 
-
-
 void setup()
 {
   // Attach the Interrupt to ISR, increasing count on high sensor
   attachInterrupt(digitalPinToInterrupt (DIST_PIN), ISR_count, RISING);
   Serial.begin(9600);
-  delay(2000);
-  Serial.println(slotC);
-
-  
+  delay(1000);
 }
 
-
 void loop(){
-
-
+  Serial.println(slotC);
 }
