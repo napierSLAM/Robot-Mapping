79d3f1b4ea2ad0ffc0cb991526b74c1a38b4398d
napierSLAM <37292377+napierSLAM@users.noreply.github.com>
2018-04-12 10:17:25 +0100

    Update servo_test.ino

--- servo/servo_test.ino
+++ servo/servo_test.ino
@@ -1,1 +1,19 @@
-to write
+/*
+ * Napier SLAM Robot
+ * 10/04/18
+ * Paddy
+ * Very simple servo function check
+ */
+#include <Servo.h> 
+
+Servo myservo;
+
+void setup() 
+{ 
+  myservo.attach(4);
+  myservo.write(90);  // set servo to mid-point
+} 
+
+void loop() {} 
+
+// we can use myservo.write(0) and myservo.write(180) for readings
