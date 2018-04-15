ebae2675bf819ad8ee0ab56b87b77203f1e2a1cd
napierSLAM <37292377+napierSLAM@users.noreply.github.com>
2018-04-15 11:23:56 +0100

    Update dataGen.ino

--- mapping/dataGen.ino
+++ mapping/dataGen.ino
@@ -1,41 +1,44 @@
+/*
+ * 15/4/18
+ * NapierSLAM
+ * Paddy
+ * Coord generator
+ * Sends a stream of tokens down serial until a rsponse is recieved
+ * Then generates and sends x,y coords down the serial with * as a seperator
+ * To be safer, should change a flag on contact but meh
+ */
+
+
 int x;
 int y;
 String data;
 String sep = "*";
 int count = 0;
-int go = 0;
-
 
 void setup() {
   Serial.begin(9600);
   establishContact();
-
 }
 
 void loop() {
-  if (count < 1) {
-    for (int i = 32; i <= 320; i = i + 32) {
+  if (count < 1) { // One set of data
+    for (int i = 32; i <= 320; i = i + 32) { 
       y = i; 
       for (int c = 64; c <= 320; c = c + 64) {
         x = c;
-        data = x + sep + y;
-        Serial.println(data);
+        data = x + sep + y; // create string
+        Serial.println(data); // send down line
       }
       count = count + 1;
     }
   }
 }
 
-
 void establishContact() {
-  while (Serial.available() <= 0) {
-    Serial.println('A');   // send a capital A
+  while (Serial.available() <= 0) { // If nothing heard
+    Serial.println('A');   // send stream of tokens
     delay(300);
   }
-  go = 1;
-  delay(500);
+  delay(500); // Give some time to settle
 }
-
-
-
-
+// This to shall pass
