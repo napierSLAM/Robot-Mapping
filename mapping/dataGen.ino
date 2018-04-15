fca8bee8a8af93d515be93058a7e3180adcd7b8e
napierSLAM <37292377+napierSLAM@users.noreply.github.com>
2018-04-15 10:48:33 +0100

    Add files via upload

---
+++ mapping/dataGen.ino
@@ -0,0 +1,41 @@
+int x;
+int y;
+String data;
+String sep = "*";
+int count = 0;
+int go = 0;
+
+
+void setup() {
+  Serial.begin(9600);
+  establishContact();
+
+}
+
+void loop() {
+  if (count < 1) {
+    for (int i = 32; i <= 320; i = i + 32) {
+      y = i; 
+      for (int c = 64; c <= 320; c = c + 64) {
+        x = c;
+        data = x + sep + y;
+        Serial.println(data);
+      }
+      count = count + 1;
+    }
+  }
+}
+
+
+void establishContact() {
+  while (Serial.available() <= 0) {
+    Serial.println('A');   // send a capital A
+    delay(300);
+  }
+  go = 1;
+  delay(500);
+}
+
+
+
+
