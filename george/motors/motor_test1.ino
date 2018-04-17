4cc7d5a576b7415e15f6de5bf210c3187a503972
geo1983 <31955203+geo1983@users.noreply.github.com>
2018-04-17 23:49:03 +0100

    testing motors

---
+++ george/motors/motor_test1.ino
@@ -0,0 +1,40 @@
+/*
+ * Mapping Robot
+ * George Oda
+ * Testing motors
+ * 
+ */
+
+#define ENABLE 5
+#define DIRA 3
+#define DIRB 4
+
+#define ENABLE1 8
+#define DIRA1 6
+#define DIRB1 7
+
+int i;
+ 
+void setup() {
+  //---set pin direction
+  pinMode(ENABLE,OUTPUT);
+  pinMode(DIRA,OUTPUT);
+  pinMode(DIRB,OUTPUT);
+  pinMode(ENABLE1,OUTPUT);
+  pinMode(DIRA1,OUTPUT);
+  pinMode(DIRB1,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  //---back and forth example
+    Serial.println("One way, then reverse");
+    digitalWrite(ENABLE,50); // enable on
+    digitalWrite(ENABLE1,HIGH); // enable on
+    
+    digitalWrite(DIRA,HIGH); //one way
+    digitalWrite(DIRA1,LOW); //one way
+    digitalWrite(DIRB,LOW);    
+    digitalWrite(DIRB1,HIGH);
+}
+  
---
+++ george/motors/turn_test.ino
@@ -0,0 +1,57 @@
+/*
+ * Mapping Robot
+ * George Oda
+ * Test turning
+ * 
+ */
+ 
+#define MRSPEED 5 // motor right speed
+#define MRDIRA 3 // motor right direction a
+#define MRDIRB 4 // motor right dir b
+
+#define MLSPEED 8 // motor left speed
+#define MLDIRA 6 // motor left dir a
+#define MLDIRB 7 // motor left dir b
+
+
+
+
+
+
+
+void setup() {
+  pinMode(MRSPEED,OUTPUT);
+  pinMode(MRDIRA,OUTPUT);
+  pinMode(MRDIRB,OUTPUT);
+  pinMode(MLSPEED,OUTPUT);
+  pinMode(MLDIRA,OUTPUT);
+  pinMode(MLDIRB,OUTPUT);
+  Serial.begin(9600);  
+  delay(2000);
+
+}
+
+void loop() {
+  TURNLEFT();
+delay(400);
+  STOP();
+  delay(3000);
+}
+
+
+void TURNLEFT(){
+    Serial.println("Turn Left");    
+    analogWrite(MRSPEED,160); // enable on
+    analogWrite(MLSPEED,160); // enable on
+    digitalWrite(MRDIRA,HIGH); //one way
+    digitalWrite(MLDIRA,LOW); //one way
+    digitalWrite(MRDIRB,LOW);    
+    digitalWrite(MLDIRB,HIGH);    
+  }
+  
+void STOP(){
+    Serial.println("Stop");    
+    digitalWrite(MRSPEED,LOW); //one way
+    digitalWrite(MLSPEED,LOW); //one way   
+    
+  }
